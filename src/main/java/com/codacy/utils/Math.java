fde4be91d0f319eb843e3ca1ec7e6f888c655127
medleycodacy <48563202+medleycodacy@users.noreply.github.com>
2022-08-10 15:58:27 +0100

    Add comment

--- src/main/java/com/codacy/utils/Math.java
+++ src/main/java/com/codacy/utils/Math.java
@@ -17,6 +17,7 @@ public class Math {
         } else {
 	    System.out.println("hey");
 	    System.out.println("hey");
+            System.out.println("hey");
             return x + y;
         }
     }
