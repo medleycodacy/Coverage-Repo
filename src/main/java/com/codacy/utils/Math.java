904cb40e4c050e21f0011099a1220f2ad13fb241
medleycodacy <48563202+medleycodacy@users.noreply.github.com>
2023-10-03 11:55:45 +0100

    remove line

--- src/main/java/com/codacy/utils/Math.java
+++ src/main/java/com/codacy/utils/Math.java
@@ -18,7 +18,6 @@ public class Math {
 	    System.out.println("hey");
 	    System.out.println("hey");
             System.out.println("hey");
-	    System.out.println("hey");
             return x + y;
         }
     }
