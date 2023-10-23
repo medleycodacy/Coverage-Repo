71071e92117b83e8174bad34fe774650cdd5e629
medleycodacy <48563202+medleycodacy@users.noreply.github.com>
2023-10-23 14:47:17 +0100

    Update Math.java

--- src/main/java/com/codacy/utils/Math.java
+++ src/main/java/com/codacy/utils/Math.java
@@ -19,6 +19,7 @@ public class Math {
 	    System.out.println("hey");
             System.out.println("hey");
 	    System.out.println("hey");
+	    System.out.println("hey");
             return x + y;
         }
     }
